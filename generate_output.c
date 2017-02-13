1d7c539f59bb5f6ceb021986222ffb1cd70d8598
Fabio Gelbcke <dietbacon@Fabios-iMac.local>
2017-02-13 15:04:39 +0100

    lunch break commit! :) wrote functions to add opcode and ACB to output lines, still not tested

--- generate_output.c
+++ generate_output.c
@@ -25,9 +25,51 @@ char			*get_opcode(char *cmd)
 	//Throw error
 }
 
+char			*param_code(char *param)
+{
+	if (param[0] == 'r')
+		return (ft_strdup("01"));
+	else if (param[0] == DIRECT_CHAR)
+		return (ft_strdup("10"));
+	else
+		return (ft_strdup("11"));
+}
+
+char			*bin_to_bytecode(char *bin)
+{
+	int			dec;
+	int			i;
+	int			mult;
+
+	dec = 0;
+	i = ft_strlen(bin) - 1;
+	mult = 1;
+	while (i >= 0)
+	{
+		dec += (bin[i] - 48) * mult;
+		mult = mult * 2;
+		i--;
+	}
+	return int_to_bytecode(dec, 1);
+}
+
 char			*get_coding_byte(char *line)
 {
-
+	char		**split_line;
+	char		**params;
+	char		*binary;
+	int			i;
+	
+	//TODO: NO ACB FOR FORK, ZJMP AND LIVE
+	split_line = ft_split_spaces(line);
+	params = ft_strsplit(split_line[sl_len - 1], SEPARATOR_CHAR);
+	i = 0;
+	binary = ft_strdup("");
+	while(params[i])
+		binary = ft_strjoin(binary, param_code(params[i++]));
+	while (ft_strlen(binary) < 8)
+		binary = ft_strjoin(binary, "00");
+	return (ft_strjoin(bin_to_bytecode(binary), ","));
 }
 
 void			generate_line(char *input_line, char **line)
