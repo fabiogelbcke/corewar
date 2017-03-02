0532c347b228e5adc645edc1f05c7d1ce9036408
Fabio Gelbcke <fabio.gelbcke@gmail.com>
2017-03-02 16:39:28 +0100

    tried removing memory leaks, now this shit doesn't work anymore

--- srcs/line_type.c
+++ srcs/line_type.c
@@ -14,70 +14,74 @@
 
 int			is_empty(char *line)
 {
-	char	**split_line;
+	int		i;
 
+	i = 0;
 	if (ft_strlen(line) == 0)
 		return (1);
-	split_line = ft_split_spaces(line);
-	if (ft_strarr_len(split_line) == 0)
-	{
-		ft_free_strarr(split_line);
-		return (1);
-	}
-	ft_free_strarr(split_line);
-	return (0);
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	if (line[i])
+		return (0);
+	return (1);
 }
 
 int			is_comment(char *line)
 {
-	char	**split_line;
+	int		i;
 
+	i = 0;
 	if (is_empty(line))
 		return (0);
-	split_line = ft_split_spaces(line);
-	if (split_line[0][0] != COMMENT_CHAR)
-	{
-		ft_free_strarr(split_line);
-		return (0);
-	}
-	ft_free_strarr(split_line);
-	return (1);
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	if (line[i] && line[i] == '#')
+		return (1);
+	return (0);
 }
 
 int			is_name(char *line)
 {
-	char	**split_line;
+	int		i;
 
+	i = 0;
 	if (is_empty(line))
 		return (0);
-	split_line = ft_split_spaces(line);
-	if (ft_strcmp(split_line[0], NAME_CMD_STRING))
-	{
-		ft_free_strarr(split_line);
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	if (!ft_startswith(&line[i], NAME_CMD_STRING))
 		return (0);
-	}
-	ft_free_strarr(split_line);
 	return (1);
 }
 
 int			is_initial_comment(char *line)
 {
-	return (!is_empty(line) && ft_startswith(line, COMMENT_CMD_STRING));
+	int		i;
+
+	i = 0;
+	if (is_empty(line))
+		return (0);
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	if (!ft_startswith(&line[i], COMMENT_CMD_STRING))
+		return (0);
+	return (1);
 }
 
 int			is_routine(char *line)
 {
 	int		is_rout;
-	char	**split_line;
-	char	*first_word;
+	int		i;
 
 	is_rout = !is_empty(line) && !is_comment(line);
 	is_rout = is_rout && !is_name(line);
 	is_rout = is_rout && !is_initial_comment(line);
-	split_line = ft_split_spaces(line);
-	first_word = split_line[0];
-	is_rout = is_rout && first_word[ft_strlen(first_word) - 1] == LABEL_CHAR;
-	ft_free_strarr(split_line);
+	i = 0;
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	while (line[i] && (line[i] != ' ' && line[i] != '\t'))
+		i++;
+	is_rout = is_rout && (i > 0 && line[i - 1] == LABEL_CHAR);
 	return (is_rout);
 }
 
